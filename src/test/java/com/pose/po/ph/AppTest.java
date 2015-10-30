57bffb9e3cb53688f4093d795f59654695d85633
chengxiaolei <chengxiaolei@tsjinrong.cn>
2015-10-30 13:10:11 +0800

    增加配置信息   初始页面ok!!

--- src/test/java/com/pose/po/ph/AppTest.java
+++ src/test/java/com/pose/po/ph/AppTest.java
@@ -1,11 +1,21 @@
 package com.pose.po.ph;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.pose.po.ph.model.User;
 
 public class AppTest {
 
 	@Test
 	public void test(){
 		
+	}
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		User u=(User) ctx.getBean("ph_user");
+		System.out.println(u.getName());
 	}
 }
